eb04ce69251b3c4ff98c7773a933aec42d9c6d80
worthlessperson3 <a454937550@163.com>
2026-01-04 22:57:46 +0800

    update

--- exercises/00_hello_world/main.cpp
+++ exercises/00_hello_world/main.cpp
@@ -4,8 +4,11 @@
 // READ: 流修饰符 <https://zh.cppreference.com/w/cpp/io/manip>
 // READ: format in cxx20 <https://zh.cppreference.com/w/cpp/utility/format/format>
 
+using namespace std;
+
 int main(int argc, char **argv) {
     // TODO: 在控制台输出 "Hello, InfiniTensor!" 并换行
-    std::cout : "Hello, InfiniTensor!" + std::endl;
+    cout << "Hello, InfiniTensor!" << endl;
+    //std::cout : "Hello, InfiniTensor!" + std::endl;
     return 0;
 }
